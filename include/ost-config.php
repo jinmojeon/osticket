108456f9e7a73fc912ed99fbc6c30c9b0e0e5570
jinmojeon <87407923+jinmojeon@users.noreply.github.com>
2021-08-06 16:18:16 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,10 +39,10 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','skccmysql21.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
-define('DBPASS','demo@pass123');
+define('DBUSER','demouser@skccmysql21');
+define('DBPASS','wjswlsah1!2@');
 
 # Table prefix
 define('TABLE_PREFIX','ost_');
